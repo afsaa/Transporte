699a955e651970c16b913bd4f190cee070f5d89d
Andres Saa <andressaa.dev@gmail.com>
2019-12-19 11:56:43 -0500

    Creating the PassengerCard component and its basic styles

---
+++ src/components/PassengerCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import "./styles/card.css";
+
+function PassengerCard(props) {
+    return(
+        <React.Fragment>
+            <div className="Card_container">
+                <h1>{props.fullName}</h1>
+                <h2>{props.address}</h2>
+                <h3>{props.birthday}</h3>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default PassengerCard;
