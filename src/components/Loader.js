d2da0743e39eb92d48d5242f559421ce5a2360c4
Andres Saa <andressaa.dev@gmail.com>
2019-12-20 13:16:43 -0500

    Creating and using the PageLoading and Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import loaderAnim from "../img/loader.svg";
+
+function Loader() {
+  return (
+    <div>
+      <img src={loaderAnim} alt="Loading Animation" />
+    </div>
+  );
+}
+
+export default Loader;
--- src/components/PassengerList.js
+++ src/components/PassengerList.js
@@ -1,59 +1,61 @@
-import React from 'react';
-import PassengerCard from './PassengerCard';
-
-import './styles/cardList.css';
+import React from "react";
+import PassengerCard from "./PassengerCard";
+import PageLoading from "./PageLoading";
+import "./styles/cardList.css";
 
 class PassengerList extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            loading: true,
-            error: null,
-            passengers: undefined
-        }
-    }
-    
-    componentDidMount() {
-        this.fetchData();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      error: null,
+      passengers: undefined
+    };
+  }
 
-    fetchData = async () => {
-        this.setState({ loading: true });
-        try {
-            const res = await fetch('http://localhost:8080/api/pasajeros');
-            const data = await res.json();
-            if (!res.ok) {
-                throw Error(res.statusText); 
-            }
-            this.setState({ loading: false, passengers: data })
-        } catch (error) {
-            this.setState({ loading:false, error: error })
-        }
-    }
+  componentDidMount() {
+    this.fetchData();
+  }
 
-    render() {
-        if (this.state.loading) {
-            return <h1>Loading ...</h1>;
-        }
-        if (this.state.error) {
-            return <h1>Shit happends!</h1>;
-        }
-        return (
-            <div className="CardList_container">
-                <ul className="unstyled-list">
-                    {
-                        this.state.passengers.map( (passenger) => {
-                            return(
-                                <li key={passenger.id}>
-                                    <PassengerCard fullName={passenger.nombre} address={passenger.direccion_residencia} birthday={passenger.fecha_nacimiento} />
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </div>
-        )
+  fetchData = async () => {
+    this.setState({ loading: true });
+    try {
+      const res = await fetch("http://localhost:8080/api/pasajeros");
+      const data = await res.json();
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
+      this.setState({ loading: false, passengers: data });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
     }
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <PageLoading />;
+    }
+    if (this.state.error) {
+      return <h1>Shit happends!</h1>;
+    }
+    return (
+      <div className="CardList_container">
+        <ul className="unstyled-list">
+          {this.state.passengers.map(passenger => {
+            return (
+              <li key={passenger.id}>
+                <PassengerCard
+                  fullName={passenger.nombre}
+                  address={passenger.direccion_residencia}
+                  birthday={passenger.fecha_nacimiento}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default PassengerList;
