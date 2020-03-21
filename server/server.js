dbbb61309b9d941a38a218ac053403f9f3af55b9
Andres Saa <andressaa.dev@gmail.com>
2020-03-21 10:16:00 -0500

    Protecting passengers endpoints and deleting old unecessary routes

--- server/server.js
+++ server/server.js
@@ -20,86 +20,8 @@ app.listen(port, () => {
   console.log("Listening on port 8080...");
 });
 
-// Creating API endpoints
-
 // Passengers endpoints
-app.use("/api/v1/pasajeros", passengersRoutes);
-
-// Get 10 passengers
-app.get("/api/pasajeros", authenticateToken, (req, res) => {
-  const { name, address } = req.query;
-  db.any("SELECT * FROM info_pasajero_view LIMIT 10", [true])
-    .then(passengers => {
-      if (name) {
-        passengers = passengers.filter(p => p.nombre === name);
-      }
-      if (address) {
-        passengers = passengers.filter(p => p.direccion_residencia === address);
-      }
-      res.json(
-        //passengers.filter(passenger => passenger.nombre === req.user.username)
-        { success: true, data: passengers }
-      );
-    })
-    .catch(error => {
-      res
-        .sendStatus(500)
-        .json({ success: false, error: "Internal server error" });
-      console.log("Error: ", error);
-    });
-});
-
-// Add a new passenger
-app.post("/api/pasajeros", authenticateToken, (req, res) => {
-  const { name, address, birthday } = req.body;
-
-  db.any(
-    `INSERT INTO pasajero 
-	VALUES('${name}', '${address}', '${birthday}')`,
-    [true]
-  )
-    .then(() => {
-      //res.json(data);
-      res.sendStatus(200).json({ success: "Passenger created successfully!" });
-    })
-    .catch(error => {
-      res.sendStatus(500).json({ error: "Internal server error" });
-      //console.log("Error: ", error);
-    });
-  res.json(req.body);
-});
-
-// Update passenger
-app.put("/api/pasajeros/:id", authenticateToken, (req, res) => {
-  const { name, address, birthday } = req.body;
-  db.any(
-    `UPDATE pasajero SET nombre='${name}', direccion_residencia='${address}', fecha_nacimiento='${birthday}' WHERE id=${req.params.id}`,
-    [true]
-  )
-    .then(() => {
-      res.sendStatus(200).json({
-        success: `Passenger with id ${req.params.id} updated successfully!`
-      });
-    })
-    .catch(error => {
-      res.sendStatus(400).json({ error: "We couldn't find that passenger" });
-      console.log(error);
-    });
-});
-
-// Delete a passenger
-app.delete("/api/pasajeros/:id", authenticateToken, (req, res) => {
-  db.any(`DELETE from pasajero WHERE id=${req.params.id}`, [true])
-    .then(() => {
-      res.sendStatus(200).json({
-        success: `Passenger with id ${req.params.id} deleted successfully!`
-      });
-    })
-    .catch(error => {
-      res.sendStatus(400).json({ error: "We couldn't find that passenger" });
-      console.log(error);
-    });
-});
+app.use("/api/v1/pasajeros", authenticateToken, passengersRoutes);
 
 //Authenticate user
 app.post("/authentication", (req, res) => {
