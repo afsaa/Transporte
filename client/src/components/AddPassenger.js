2d46f659407d040fba5831d47d83db9a9a665f0f
Andres Saa <andressaa.dev@gmail.com>
2020-03-29 22:06:46 -0500

    Installing and using material UI to style the form and submit button

--- client/src/components/AddPassenger.js
+++ client/src/components/AddPassenger.js
@@ -1,11 +1,25 @@
 import React, { Fragment, useState, useEffect, useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch"
+    }
+  }
+}));
 
 function AddPassenger() {
   const { JWT, getToken, addPassenger } = useContext(GlobalContext);
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [birthday, setBirthday] = useState("");
+
+  const classes = useStyles();
 
   useEffect(() => {
     if (!JWT) {
@@ -28,27 +42,39 @@ function AddPassenger() {
   return (
     <Fragment>
       <h1>Fill the form with your data to join us!</h1>
-      <form onSubmit={e => handleSubmit(e)}>
-        <input
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={e => handleSubmit(e)}
+      >
+        <TextField
+          label="Your name"
           type="text"
           name="name"
           onChange={e => setName(e.target.value)}
-          placeholder="Your name"
         />
-        <input
+        <TextField
+          label="Your address"
           type="text"
           name="address"
           onChange={e => setAddress(e.target.value)}
-          placeholder="Your address"
         />
-        <input
+        <TextField
+          label="Your birthday"
           type="text"
           name="birthday"
           onChange={e => setBirthday(e.target.value)}
-          placeholder="Your birthday"
         />
         <br />
-        <button type="submit">Join the fleet</button>
+        <Button
+          type="submit"
+          variant="contained"
+          size="large"
+          className={classes.margin}
+        >
+          Join the fleet
+        </Button>
       </form>
     </Fragment>
   );
